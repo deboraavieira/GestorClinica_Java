e99d95ddfae864040cdedf48f65b4fc53fcab409
Débora Vieira <deboraluizafono@gmail.com>
2024-10-27 16:04:20 +0000

    Add Financeiro form

--- src/projfinal/projFinal.java
+++ src/projfinal/projFinal.java
@@ -6,6 +6,8 @@ package projfinal;
 
 import gestaoclinicafono.grafica.JFrameMain;
 import gestaoclinicafono.grafica.Login;
+import gestaoclinicafono.grafica.Prontuarios;
+import gestaoclinicafono.grafica.RegistaFono;
 import gestaoclinicafono.grafica.RegistaPaciente;
 
 /**
@@ -14,15 +16,37 @@ import gestaoclinicafono.grafica.RegistaPaciente;
  */
 public class projFinal {
     public static void main(String[]args){
-         
-            JFrameMain jfm2=new JFrameMain();
-            jfm2.setVisible(true);
+         try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(RegistaFono.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(RegistaFono.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(RegistaFono.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(RegistaFono.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
             
             Login LoginFrame = new Login();
             LoginFrame.setVisible(true);
             
+            JFrameMain jfm2=new JFrameMain();
+            jfm2.setVisible(true);
+            
             RegistaPaciente jrp1 = new RegistaPaciente();
             jrp1.setVisible(true);
+            
+            RegistaFono jrf1 = new RegistaFono();
+            jrf1.setVisible(true);
+            
+            Prontuarios jfpro = new Prontuarios();
+            jfpro.setVisible(true);
             
         } 
     
